16a3a6562748741cdce5c74959df44d9b8eea0a7
刘鹏涛 <1455682411@qq.com>
2017-08-14 17:38:16 +0800

    第一个springboot项目

--- mvnw.cmd
+++ mvnw.cmd
@@ -43,7 +43,7 @@ if "%HOME%" == "" (set "HOME=%HOMEDRIVE%%HOMEPATH%")
 
 @REM Execute a user defined script before this one
 if not "%MAVEN_SKIP_RC%" == "" goto skipRcPre
-@REM check for pre script, once with legacy .bat ending and once with .cmd ending
+@REM check for pre script, once with legacy .bat ending and once with .cmd ending5
 if exist "%HOME%\mavenrc_pre.bat" call "%HOME%\mavenrc_pre.bat"
 if exist "%HOME%\mavenrc_pre.cmd" call "%HOME%\mavenrc_pre.cmd"
 :skipRcPre
